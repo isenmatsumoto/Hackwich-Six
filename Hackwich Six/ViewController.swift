0937e59804eb756426a28165f96a0eeb21cac270
isenmatsumoto <isenmats@hawaii.edu>
2022-03-02 22:06:03 -1000

    Partially Completed Problem Set 1

--- Hackwich Six/ViewController.swift
+++ Hackwich Six/ViewController.swift
@@ -10,27 +10,37 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var myFriendsArray = ["Sara", "Nicole", "Grant"]
-    
+    var myCountryArray = ["Switzerland", "St. Lucia", "Guam", "Hong Kong", "New York City"]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return myFriendsArray.count
+        return myCountryArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
-        
         let text = myFriendsArray[indexPath.row]
         cell?.textLabel?.text = text
         return cell!
+        
     }
     
-
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
