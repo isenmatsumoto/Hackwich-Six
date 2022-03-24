49dcf275c4258e906c553cd0bfcbb525d3caafa9
isenmatsumoto <isenmats@hawaii.edu>
2022-03-23 22:30:34 -1000

    Finished All Parts

--- Hackwich Six/ViewController.swift
+++ Hackwich Six/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var myFriendsArray = ["Sara", "Nicole", "Grant"]
+    var myFriendsArray = ["Kapolei Kalapawai"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var myCountryArray = ["Switzerland", "St. Lucia", "Guam", "Hong Kong", "New York City"]
-    var friendsHomeArray = ["Ewa Beach", "Mililani"]
+    var friendsHomeArray = ["Kapolei"]
+    
+    var restaurantImageData = [String]()
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "Propert List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+
+        restaurantImageData = dict!.object(forKey: "restaurantImages")
+        as![String]
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return myFriendsArray.count
@@ -28,13 +52,14 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
         
     }
     
-    
-    
-    
-override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let s1 = segue.destination as! detailViewController
+        let imageIndex = tableView.indexPathForSelectedRow?.row
+        s1.imagePass = restaurantImageData[imageIndex!]
     }
+    
+    
 
 
 
